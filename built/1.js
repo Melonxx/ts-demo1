544cc16546539e41fbf2dc2d79ccbb73cb7ab8b2
zhangchao <529743595@qq.com>
2019-05-07 14:00:09 +0800

    npm init+demo编写失败(ts文件并未在html中成功执行)

---
+++ built/1.js
@@ -0,0 +1,15 @@
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports", "jquery"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var $ = require("jquery");
+    $('#app').html('111111111111');
+});
+//# sourceMappingURL=1.js.map
